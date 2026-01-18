565a11eebe31ddde38c944d539455cdeb9b2d211
kmsanjar47 <khan.md.saifullah@g.bracu.ac.bd>
2026-01-18 22:01:46 +0600

    uppercase added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const titels = document.querySelector("h1");
 titels.classList.add("red");
 titels.classList.add("new");
-// titels.classList.add('uppercas')
+titels.classList.add("uppercas");
 // titels.classList.remove('uppercase')
