6c0a91e1df781bf6dd178aa43e18bc49fe8af55c
kmsanjar47 <khan.md.saifullah@g.bracu.ac.bd>
2026-01-18 22:55:20 +0600

    Added a console log from dev two

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const titels = document.querySelector("h1");
 titels.classList.add("red");
 titels.classList.add("new");
+console.log(titels.classList.contains("red"));
 // titels.classList.add('uppercas')
 // titels.classList.remove('uppercase')
 titels.classList.toggle("uppercase");
