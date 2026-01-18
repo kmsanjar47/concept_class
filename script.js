9dc7b77c7865d581ceebe913717b6e5079140831
kmsanjar47 <khan.md.saifullah@g.bracu.ac.bd>
2026-01-18 21:49:05 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const titels = document.querySelector("h1");
+titels.classList.add("red");
+// titels.classList.add('new')
+// titels.classList.add('uppercas')
+// titels.classList.remove('uppercase')
